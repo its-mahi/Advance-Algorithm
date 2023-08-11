f52462fbcda4e1aa7f8ef7ebddd8626b641f6879
its-mahi <104795672+its-mahi@users.noreply.github.com>
2023-08-11 09:41:29 +0530

    Create "horsepools_string_matching_algo.cpp"
    Code of Advance Algorithm Lab 5

---
+++ Lab 5/horsepools_string_matching_algo.cpp
@@ -0,0 +1,65 @@
+/**
+ * @author : its_mahi
+ * Code : Horsepools String Matching Algorithm
+ * 
+ * Desc : two string "text" and "pattern" are there. we have to find is there "pattern" exist in "text" string. it must be a continuous(substring). not subsequence.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    string text;
+    cout << "Enter your text : ";
+    cin >> text;
+    
+    string pattern;
+    cout << "Enter your pattern to find in text : ";
+    cin >> pattern;
+    
+    int n = text.size();
+    int m = pattern.size();
+    
+    map<char, int> shift_table;
+    bool foo = false;
+    
+    // Creating Shift Table
+    int k = m-1;
+    for(int i=0 ; i<m-1 ; i++) {
+        shift_table[pattern[i]] = k;
+        k--;
+    }
+    
+    for(int i=0 ; i<=n-m ; ) {
+        int p = i+m-1;
+        int q = m-1;
+        
+        while(q >= 0 && p >= 0 && pattern[q] == text[p]) {
+            p--;
+            q--;
+        }
+        
+        if(q == -1) {
+            cout << "Pattern matched at : " << i << endl;
+            i++;
+            foo = true;
+        }
+        else {
+            if(shift_table[text[i+m-1]] > 0) {
+                i += shift_table[text[i+m-1]];
+            }
+            else {
+                i += m;
+            }
+        }
+    }
+    
+    if(foo == false) {
+        cout << "No match is present" << endl;
+    }
+    
+    cout << "computation is done" << endl;
+    
+    return 0;
+}
